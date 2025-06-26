01c127ca5ed336facf0b3273cfaf60ae83270169
nils_kiefer <21311514+nilskiefer@users.noreply.github.com>
2025-06-26 15:20:35 +0200

    Move driver to bosch namespace

--- drivers/sensor/bosch/bmx160/bmx160.c
+++ drivers/sensor/bosch/bmx160/bmx160.c
@@ -1,4 +1,12 @@
+/* Bosch BMX160 9-axis sensor driver
+ *
+ * Copyright (c) 2023 Infineon Technologies AG
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include "bmx160.h"
+#include <string.h>
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(bmx160, CONFIG_SENSOR_LOG_LEVEL);
@@ -11,8 +19,7 @@ static int bmx160_sample_fetch(const struct device *dev, enum sensor_channel cha
     /* Read acceleration, gyroscope and magnetometer data from sensor */
     uint8_t buf[20];
 
-    /* This driver expects BMI160 + BMM150 data block starting at accel registers. */
-    if (i2c_burst_read_dt(&cfg->i2c, 0x12, buf, sizeof(buf)) < 0) {
+    if (i2c_burst_read_dt(&cfg->i2c, BMX160_REG_DATA_START, buf, sizeof(buf)) < 0) {
         return -EIO;
     }
 
@@ -44,15 +51,24 @@ static int bmx160_channel_get(const struct device *dev, enum sensor_channel chan
     struct bmx160_data *data = dev->data;
 
     switch (chan) {
+    case SENSOR_CHAN_ACCEL_XYZ:
+        memcpy(val, data->accel, sizeof(data->accel));
+        return 0;
     case SENSOR_CHAN_ACCEL_X:
     case SENSOR_CHAN_ACCEL_Y:
     case SENSOR_CHAN_ACCEL_Z:
         *val = data->accel[chan - SENSOR_CHAN_ACCEL_X];
         return 0;
+    case SENSOR_CHAN_GYRO_XYZ:
+        memcpy(val, data->gyro, sizeof(data->gyro));
+        return 0;
     case SENSOR_CHAN_GYRO_X:
     case SENSOR_CHAN_GYRO_Y:
     case SENSOR_CHAN_GYRO_Z:
         *val = data->gyro[chan - SENSOR_CHAN_GYRO_X];
+        return 0;
+    case SENSOR_CHAN_MAGN_XYZ:
+        memcpy(val, data->magn, sizeof(data->magn));
         return 0;
     case SENSOR_CHAN_MAGN_X:
     case SENSOR_CHAN_MAGN_Y:
@@ -73,14 +89,42 @@ static int bmx160_init(const struct device *dev)
     }
 
     /* Reset sensor */
-    uint8_t cmd[2] = {0x7E, 0xB6};
+    uint8_t cmd[2] = {BMX160_REG_CMD, BMX160_CMD_SOFT_RESET};
     if (i2c_write_dt(&cfg->i2c, cmd, sizeof(cmd)) < 0) {
         return -EIO;
     }
 
     k_msleep(100);
 
-    /* Additional configuration would normally be done here */
+    uint8_t chip;
+    if (i2c_reg_read_byte_dt(&cfg->i2c, BMX160_REG_CHIPID, &chip) < 0) {
+        return -EIO;
+    }
+
+    if (chip != BMX160_CHIP_ID) {
+        LOG_ERR("Unexpected chip id 0x%x", chip);
+        return -ENODEV;
+    }
+
+    if (i2c_reg_write_byte_dt(&cfg->i2c, BMX160_REG_ACC_RANGE, BMX160_DEFAULT_RANGE_ACC) < 0) {
+        return -EIO;
+    }
+
+    if (i2c_reg_write_byte_dt(&cfg->i2c, BMX160_REG_GYR_RANGE, BMX160_DEFAULT_RANGE_GYR) < 0) {
+        return -EIO;
+    }
+
+    if (i2c_reg_update_byte_dt(&cfg->i2c, BMX160_REG_ACC_CONF,
+                               BMX160_ACC_CONF_ODR_MASK,
+                               BMX160_DEFAULT_ODR_ACC) < 0) {
+        return -EIO;
+    }
+
+    if (i2c_reg_update_byte_dt(&cfg->i2c, BMX160_REG_GYR_CONF,
+                               BMX160_GYR_CONF_ODR_MASK,
+                               BMX160_DEFAULT_ODR_GYR) < 0) {
+        return -EIO;
+    }
 
     return 0;
 }
